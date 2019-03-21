b615d9fa9b56e8fb52c54acfead56d9a1b3b3c2b
yabing <yabing@sfu.ca>
2019-03-21 05:42:22 -0700

    add observer code snippets

---
+++ observer/start from scratch/WeatherStation.java
@@ -0,0 +1,11 @@
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData wd = new WeatherData();
+        
+        CurrentConditionsDisplay ccd = new CurrentConditionsDisplay(wd);
+        
+        wd.setMeasurements(80,65,30.4f);
+        wd.setMeasurements(82,70,29.2f);
+        wd.setMeasurements(78,90,29.2f);
+    }
+}
